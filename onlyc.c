1990ba6f8b085220cfab1bb569df30ac9cfee355
RasitAkay11 <50198490+RasitAkay11@users.noreply.github.com>
2021-04-14 18:20:08 +0200

    Update onlyc.c

--- onlyc.c
+++ onlyc.c
@@ -8,19 +8,52 @@ int main()
     char player[6][10];
     int gok[6];
     srand (time(NULL));
-    int rnd = rand() % 100 + 1;
+    int rnd = rand() % 100 + 1, highest, lowest;
 
     printf("Fill in the 6 player names:\n");
     for(int i = 0; i < 6; i++){
         //printf("What is your name? ");
         scanf("%s", &player[i][10]);
-        printf("%s joined the party!\n", player[i+1]);
+        //printf("%s joined the party!\n", player[i+1]);
     }
-
+    printf("Welcome to the party %s, %s, %s, %s, %s, %s!\n", player[1], player[2], player[3], player[4], player[5], player[6]);
     for(int i = 0; i < 6; i++){
         printf("%s, what is your guess? ", player[i+1]);
         scanf("%d", &gok[i]);
     }
+
+    for(int i = 0; i < 5; i++){
+        gok[i] = gok[i] - rnd;
+    }
+
+    highest = gok[0];
+    lowest = gok[0];
+
+    for(int i = 0; i < 5; i++)
+    {
+        if(gok[i] < gok[i+1])
+        {
+            highest = gok[i+1];
+        }
+        if(gok[i] > gok[i+1])
+        {
+            lowest = gok[i+1]; //Lowest werkt niet zonder lijn 25 en 26, gaat nooit in de if en neemt een radnom nummer.
+        }
+    }
+
+    int verste = highest + lowest;
+    if(verste > 0){
+        printf("%d is het verste er van af!\n", highest);
+    }
+    else if(verste < 0){
+        printf("%d is het verste er van af!\n", lowest);
+    }
+    else{
+        printf("Ze liggen er even ver van af!\n");
+    }
+    //printf("Dit is de hoogste gok %d\n", highest);
+    //printf("Dit is de laagste gok %d\n", lowest);
+
 
 
     /*if(gok == rnd){
