8f650a1a62a65c3087a151f2345788f570191cc9
RasitAkay11 <50198490+RasitAkay11@users.noreply.github.com>
2021-05-20 20:46:00 +0200

    Update server.c

--- server.c
+++ server.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 
 char *strsep(char **stringp, const char *delim) {
     char *rv = *stringp;
@@ -15,7 +17,6 @@ char *strsep(char **stringp, const char *delim) {
             *stringp = 0; }
     return rv;
 }
-
 char *parse(int keer, char *ParseString)
 {
     char *String,*ParsedString;
@@ -29,51 +30,55 @@ char *parse(int keer, char *ParseString)
 
 int main ( int argc, char * argv[] )
 {
-    //bericht
-    const char *BerichtGok = (argc > 1)? argv [1]: "guessit>gok!>";
+    //filter
+    const char *FilterGok = (argc > 1)? argv [1]: "guessit>gok!>";
 
-    char gok[10];
-    char sendgok[100];
-    char buffer[256];
+    //variabelen
+    srand(time(NULL));
+    int rnd = rand() % 100 + 1, guessplayer1;
+    char buffer [256];
     char *ParsedString;
+
+    //connect
     void *context = zmq_ctx_new();
     void *publisher = zmq_socket(context, ZMQ_PUSH);
     void *subscriber = zmq_socket (context, ZMQ_SUB);
-
-    //intro
-    printf("The rules are easy.. You will race against 5 other people!\nThe person who will be the closest to the random number every 6 rounds wins! Except if someone already guesses it!\n\nMaking connection with the server...\n");
-
-    //connect
     int rp = zmq_connect(publisher, "tcp://benternet.pxl-ea-ict.be:24041");
-    int rs = zmq_connect(subscriber, "tcp://benternet.pxl-ea-ict.be:24042" );
-
-    sleep (1);
+    int rs = zmq_connect(subscriber, "tcp://benternet.pxl-ea-ict.be:24042");
 
     //check if connect failed
     if (rp != 0 && rs != 0)
     {
         printf("ERROR: ZeroMQ error occurred during zmq_ctx_new(): %s\n", zmq_strerror(errno));
+
         return EXIT_FAILURE;
-    } else{
-        printf("Connection has been made! Goodluck!\n\n");
+    }
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok, 8);
+
+    while (1) {
+        //ontvang gok
+        memset(buffer,0,256);
+        zmq_recv (subscriber, buffer, 256, 0);
+        ParsedString = parse(3, buffer);
+        printf("The player has guessed %s\n", ParsedString);
+
+        //stuur restultaat
+        guessplayer1 = atoi(ParsedString);
+        printf("gok is %d\n", guessplayer1);
+        printf("nummer is %d\n", rnd);
+        if(guessplayer1 < rnd){
+            zmq_send(publisher, "guessit>gok?>Your guess is lower then the number", 49,0);
+        } else if(guessplayer1 > rnd){
+            zmq_send(publisher, "guessit>gok?>Your guess is higher then the number", 50,0);
+        } else if(guessplayer1 == rnd){
+            zmq_send(publisher, "guessit>gok?>U WON!", 20,0);
+        } else{
+            zmq_send(publisher, "guessit>gok?>something wrong!", 30,0);
+        }
+        break;
     }
 
-    rs = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "guessit>gok?>", 13);
-
-    //verzend uw gok naar server
-    printf("What is your guess? ");
-    scanf("%s", &gok);
-    strcpy(sendgok, BerichtGok);
-    strcat(sendgok, gok);
-    zmq_send (publisher, sendgok, strlen(sendgok), 0);
-
-    //ontvang resultaat
-    memset(buffer,0,256);
-    rs = zmq_recv(subscriber, buffer, 256,0);
-    ParsedString = parse(3, buffer);
-    printf("%s", ParsedString);
-
-    zmq_close (subscriber);
+    zmq_close (publisher);
     zmq_ctx_destroy (context);
     return 0;
 }
