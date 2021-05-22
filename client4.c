ca6ab39b9e8f455a216610d588e77a252eee98fa
RasitAkay11 <50198490+RasitAkay11@users.noreply.github.com>
2021-05-22 17:18:53 +0200

    Update client4.c

--- client4.c
+++ client4.c
@@ -67,6 +67,7 @@ int main ( int argc, char * argv[] )
     strcpy(sendnaam, BerichtNaam);
     strcat(sendnaam, naam);
     zmq_send(publisher, sendnaam, strlen(sendnaam), 0);
+    zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, "guessit>naam?>", 14);
 
     rs = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "guessit>gok4?>", 14);
 
