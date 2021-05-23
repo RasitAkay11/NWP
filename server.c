bf12d9647d801af68f6f4bece679ff0f07058d76
RasitAkay11 <50198490+RasitAkay11@users.noreply.github.com>
2021-05-24 01:21:09 +0200

    Update server.c

--- server.c
+++ server.c
@@ -30,14 +30,6 @@ char *parse(int keer, char *ParseString)
 
 int main ( int argc, char * argv[] )
 {
-    //filter naam
-    const char *FilterNaam = (argc > 1)? argv [1]: "guessit>naam!>";
-    const char *FilterNaam2 = (argc > 1)? argv [1]: "guessit>naam2!>";
-    const char *FilterNaam3 = (argc > 1)? argv [1]: "guessit>naam3!>";
-    const char *FilterNaam4 = (argc > 1)? argv [1]: "guessit>naam4!>";
-    const char *FilterNaam5 = (argc > 1)? argv [1]: "guessit>naam5!>";
-    const char *FilterNaam6 = (argc > 1)? argv [1]: "guessit>naam6!>";
-
     //filter gok
     const char *FilterGok = (argc > 1)? argv [1]: "guessit>gok!>";
     const char *FilterGok2 = (argc > 1)? argv [1]: "guessit>gok2!>";
@@ -47,12 +39,17 @@ int main ( int argc, char * argv[] )
     const char *FilterGok6 = (argc > 1)? argv [1]: "guessit>gok6!>";
 
     //variabelen
-    int gok[6], highest = -100, lowest = 100, h, l, r; //h = highest player l = lowest player
-    char *player[6][10];
     srand(time(NULL));
-    int rnd = rand() % 100 + 1;
-    char buffer [256];
-    char *ParsedString;
+    int gok[6], rnd = rand() % 100 + 1,highest = -100, charlentgh, lowest = 100, h, l; //h = highest player l = lowest player
+    char *VraagGok [6][36], buffer[256], *ParsedString;
+
+    //verzendlijst
+    VraagGok[0][34] = "guessit>gok?>What is your guess?: ";
+    VraagGok[1][35] = "guessit>gok2?>What is your guess?: ";
+    VraagGok[2][35] = "guessit>gok3?>What is your guess?: ";
+    VraagGok[3][35] = "guessit>gok4?>What is your guess?: ";
+    VraagGok[4][35] = "guessit>gok5?>What is your guess?: ";
+    VraagGok[5][35] = "guessit>gok6?>What is your guess?: ";
 
     printf("Starting the service...\n");
 
@@ -64,59 +61,47 @@ int main ( int argc, char * argv[] )
     int rs = zmq_connect(subscriber, "tcp://benternet.pxl-ea-ict.be:24042");
 
     //check if connect failed
-    if (rp != 0 && rs != 0)
-    {
+    if (rp != 0 && rs != 0){
         printf("ERROR: ZeroMQ error occurred during zmq_ctx_new(): %s\n", zmq_strerror(errno));
-
         return EXIT_FAILURE;
     } else{
         sleep(2);
         printf("The service is ready to go!\n\n");
     }
 
+    //individueel subscriben op gokken players
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok, 8);
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok2, 9);
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok3, 9);
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok4, 9);
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok5, 9);
+    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok6, 9);
+
+    //Momenteel is het de bedoeling dat de server en client1 alleen communiceren. Als client1 werkt komen de rest, dat is hoe ik werk.
+    //server stuurt vraag. client ontvangt. client stuurt antw. server ontvangt zijn eige vraag, MAAR dat is niet wat door de broker gaat. Wat er door de broker gaat klopt.
+    //Server ontvangt bij lijn 99, lijn 94. Die receive werkt precies niet.
+    //Om code te runnen -> eerst client.c runnen dan server.c
     while (1) {
+        sleep(2);
+        //Vraag een per een hun gok en ontvang gokken. Nummer ze
+        for(int i = 0; i < 1; i++){
+            if( i < 0){
+                charlentgh = 35;
+            } else{
+                charlentgh = 34;
+            }
+            //stel uw vraag aan player[i]
+            zmq_send(publisher, VraagGok[i][charlentgh], charlentgh,0);
+            //sleep(4);
 
-        //individueel subscriben op namen players
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterNaam, 10);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterNaam2, 11);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterNaam3, 11);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterNaam4, 11);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterNaam5, 11);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterNaam6, 11);
-
-        //ontvang namen
-        for(int i = 0; i < 6; i++){
+            //ontvang uw antwoord van player[i];
             memset(buffer,0,256);
             zmq_recv(subscriber, buffer, 256, 0);
-            player[i][9] = parse(3, buffer);
-            printf("Player %d's name is %s\n", i+1, player[i][9]);
-        }
-
-        //individueel unsubscriben op namen players
-        zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, FilterNaam, 10);
-        zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, FilterNaam2, 11);
-        zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, FilterNaam3, 11);
-        zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, FilterNaam4, 11);
-        zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, FilterNaam5, 11);
-        zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, FilterNaam6, 11);
-
-
-        //individueel subscriben op gokken players
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok, 8);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok2, 9);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok3, 9);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok4, 9);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok5, 9);
-        zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, FilterGok6, 9);
-
-        //ontvang gokken
-        for(int i = 0; i < 6; i++){
-            memset(buffer,0,256);
-            zmq_recv (subscriber, buffer, 256, 0);
             ParsedString = parse(3, buffer);
             printf("Player %d has guessed %s\n", i+1, ParsedString);
             gok[i] = atoi(ParsedString);
         }
+
 
         printf("\nplayer 1 %d\nplayer 2 %d\nplayer3 %d\nplayer4 %d\nplayer 5 %d\nplayer 6 %d\nThe number %d\n\n", gok[0], gok[1], gok[2], gok[3], gok[4], gok[5], rnd);
 
@@ -142,21 +127,21 @@ int main ( int argc, char * argv[] )
         //uitslag bepalen.
         if(highest > 0 && lowest > 0){
             printf("\nplayer %d guessed %d, but it is the farest, bye bye!\n", h, highest+rnd);
-            r = h;
+            //r = h;
         }
         else if(highest < 0 && lowest < 0){
             printf("\nplayer %d guessed %d, but it is the farest, bye bye!\n", l, lowest+rnd);
-            r = l;
+            //r = l;
         }
         else if( (highest < 0 && lowest > 0) || (highest > 0 && lowest < 0)){
             int result = lowest + highest;
             if(result > 0){
                 printf("\nplayer %d guessed %d, but it is the farest, bye bye!\n", h, highest+rnd);
-                r = h;
+                //r = h;
             }
             else{
                 printf("\nplayer %d guessed %d, but it is the farest, bye bye!\n", l, lowest+rnd);
-                r = l;
+                //r = l;
             }
         }
         zmq_send(publisher, "guessit>gok?>You are player one.", 32,0);
@@ -166,7 +151,6 @@ int main ( int argc, char * argv[] )
         zmq_send(publisher, "guessit>gok5?>You are player five.", 35,0);
         zmq_send(publisher, "guessit>gok6?>You are player six.", 34,0);
     break;
-
     }
 
     zmq_close (publisher);
