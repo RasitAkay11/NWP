b9d007047ae1e921b977347aabcc23f3ac42c3b5
RasitAkay11 <50198490+RasitAkay11@users.noreply.github.com>
2021-05-25 22:54:26 +0200

    Update client5.c

--- client5.c
+++ client5.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 char *strsep(char **stringp, const char *delim) {
     char *rv = *stringp;
@@ -31,7 +32,8 @@ int main ( int argc, char * argv[] )
 {
     //bericht
     const char *BerichtGok = (argc > 1)? argv [1]: "guessit>gok5!>";
-
+    bool playing = true;
+    int goki = 0;
     char gok[10];
     char sendgok[100];
     char buffer[256];
@@ -44,8 +46,8 @@ int main ( int argc, char * argv[] )
     printf("The rules are easy.. You will race against 5 other people!\nThe person who will be the closest to the random number every 6 rounds wins! Except if someone already guesses it!\n\nMaking connection with the server...\n");
 
     //connect
-    int rp = zmq_connect(publisher, "tcp://benternet.pxl-ea-ict.be:24041");
-    int rs = zmq_connect(subscriber, "tcp://benternet.pxl-ea-ict.be:24042" );
+    int rp = zmq_connect(publisher, "tcp://benternet.backup.pxl-ea-ict.be:24041");
+    int rs = zmq_connect(subscriber, "tcp://benternet.backup.pxl-ea-ict.be:24042" );
 
     sleep (1);
 
@@ -60,7 +62,7 @@ int main ( int argc, char * argv[] )
 
     zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "guessit>gok5?>", 14);
 
-    while(1){
+    while(playing == true){
     //Ontvang vraag en verstuur antwoord
     memset(buffer,0,256);
     zmq_recv(subscriber, buffer, 256,0);
@@ -68,7 +70,13 @@ int main ( int argc, char * argv[] )
     printf("%s\n", ParsedString);
     printf("Fill in your guess between 0 and 100: ");
     scanf("%s", gok);
-    printf("\n");
+    goki = atoi(gok);
+    while(((goki < 0) || (goki > 100))){
+        printf("\n");
+        printf("The service said between 0 and 100 u stupid fuck try again: ");
+        scanf("%s", gok);
+        goki = atoi(gok);
+    }
     strcpy(sendgok, BerichtGok);
     strcat(sendgok, gok);
     zmq_send(publisher, sendgok, strlen(sendgok), 0);
@@ -79,6 +87,10 @@ int main ( int argc, char * argv[] )
     zmq_recv(subscriber, buffer, 256,0);
     ParsedString = parse(3, buffer);
     printf("%s\n", ParsedString);
+    if((strcmp(ParsedString, "Sadly, you lost.")) == 1){
+        zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, "guessit>gok5?>", 14);
+    }
+    printf("\n");
 }
     zmq_close (subscriber);
     zmq_ctx_destroy (context);
