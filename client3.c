6700dce6776cc927ffe53f7f30bd938d8b63ed58
RasitAkay11 <50198490+RasitAkay11@users.noreply.github.com>
2021-05-25 22:53:57 +0200

    Update client3.c

--- client3.c
+++ client3.c
@@ -4,19 +4,7 @@
 #include <string.h>
 #include <assert.h>
 #include <stdlib.h>
-
-char *strsep(char **stringp, const char *delim) {
-    char *rv = *stringp;
-    if (rv) {
-        *stringp += strcspn(*stringp, delim);
-        if (**stringp)
-            *(*stringp)++ = '\0';
-        else#include <zmq.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <assert.h>
-#include <stdlib.h>
+#include <stdbool.h>
 
 char *strsep(char **stringp, const char *delim) {
     char *rv = *stringp;
@@ -44,7 +32,8 @@ int main ( int argc, char * argv[] )
 {
     //bericht
     const char *BerichtGok = (argc > 1)? argv [1]: "guessit>gok3!>";
-
+    bool playing = true;
+    int goki = 0;
     char gok[10];
     char sendgok[100];
     char buffer[256];
@@ -57,8 +46,8 @@ int main ( int argc, char * argv[] )
     printf("The rules are easy.. You will race against 5 other people!\nThe person who will be the closest to the random number every 6 rounds wins! Except if someone already guesses it!\n\nMaking connection with the server...\n");
 
     //connect
-    int rp = zmq_connect(publisher, "tcp://benternet.pxl-ea-ict.be:24041");
-    int rs = zmq_connect(subscriber, "tcp://benternet.pxl-ea-ict.be:24042" );
+    int rp = zmq_connect(publisher, "tcp://benternet.backup.pxl-ea-ict.be:24041");
+    int rs = zmq_connect(subscriber, "tcp://benternet.backup.pxl-ea-ict.be:24042" );
 
     sleep (1);
 
@@ -73,7 +62,7 @@ int main ( int argc, char * argv[] )
 
     rs = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "guessit>gok3?>", 14);
 
-    while(1){
+    while(playing == true){
     //Ontvang vraag en verstuur antwoord
     memset(buffer,0,256);
     zmq_recv(subscriber, buffer, 256,0);
@@ -81,91 +70,27 @@ int main ( int argc, char * argv[] )
     printf("%s\n", ParsedString);
     printf("Fill in your guess between 0 and 100: ");
     scanf("%s", gok);
+    goki = atoi(gok);
+    while(((goki < 0) || (goki > 100))){
+        printf("\n");
+        printf("The service said between 0 and 100 u stupid fuck try again: ");
+        scanf("%s", gok);
+        goki = atoi(gok);
+    }
+    strcpy(sendgok, BerichtGok);
+    strcat(sendgok, gok);
+    zmq_send(publisher, sendgok, strlen(sendgok), 0);
+
+
+    //ontvang resultaat
+    memset(buffer,0,256);
+    zmq_recv(subscriber, buffer, 256,0);
+    ParsedString = parse(3, buffer);
+    printf("%s\n", ParsedString);
+    if((strcmp(ParsedString, "Sadly, you lost.")) == 1){
+        zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, "guessit>gok3?>", 14);
+    }
     printf("\n");
-    strcpy(sendgok, BerichtGok);
-    strcat(sendgok, gok);
-    zmq_send(publisher, sendgok, strlen(sendgok), 0);
-
-
-    //ontvang resultaat
-    memset(buffer,0,256);
-    zmq_recv(subscriber, buffer, 256,0);
-    ParsedString = parse(3, buffer);
-    printf("%s\n", ParsedString);
-
-    }
-    zmq_close (subscriber);
-    zmq_ctx_destroy (context);
-    return 0;
-}
-
-            *stringp = 0; }
-    return rv;
-}
-
-char *parse(int keer, char *ParseString)
-{
-    char *String,*ParsedString;
-    String = strdup(ParseString);
-    for (int i = 0; i < keer; i++)
-    {
-        ParsedString = strsep(&String, ">");
-    }
-    return ParsedString;
-}
-
-int main ( int argc, char * argv[] )
-{
-    //bericht
-    const char *BerichtGok = (argc > 1)? argv [1]: "guessit>gok3!>";
-
-    char gok[10];
-    char sendgok[100];
-    char buffer[256];
-    char *ParsedString;
-    void *context = zmq_ctx_new();
-    void *publisher = zmq_socket(context, ZMQ_PUSH);
-    void *subscriber = zmq_socket (context, ZMQ_SUB);
-
-    //intro
-    printf("The rules are easy.. You will race against 5 other people!\nThe person who will be the closest to the random number every 6 rounds wins! Except if someone already guesses it!\n\nMaking connection with the server...\n");
-
-    //connect
-    int rp = zmq_connect(publisher, "tcp://benternet.pxl-ea-ict.be:24041");
-    int rs = zmq_connect(subscriber, "tcp://benternet.pxl-ea-ict.be:24042" );
-
-    sleep (1);
-
-    //check if connect failed
-    if (rp != 0 && rs != 0)
-    {
-        printf("ERROR: ZeroMQ error occurred during zmq_ctx_new(): %s\n", zmq_strerror(errno));
-        return EXIT_FAILURE;
-    } else{
-        printf("Connection has been made! Goodluck!\n\n");
-    }
-
-    rs = zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "guessit>gok3?>", 14);
-
-    while(1){
-    //Ontvang vraag en verstuur antwoord
-    memset(buffer,0,256);
-    zmq_recv(subscriber, buffer, 256,0);
-    ParsedString = parse(3, buffer);
-    printf("%s\n", ParsedString);
-    printf("Fill in your guess between 0 and 100: ");
-    scanf("%s", gok);
-    strcpy(sendgok, BerichtGok);
-    strcat(sendgok, gok);
-    zmq_send(publisher, sendgok, strlen(sendgok), 0);
-
-
-    //ontvang resultaat
-    memset(buffer,0,256);
-    zmq_recv(subscriber, buffer, 256,0);
-    ParsedString = parse(3, buffer);
-    printf("%s", ParsedString);
-
     }
     zmq_close (subscriber);
     zmq_ctx_destroy (context);
