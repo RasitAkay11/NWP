55fd3e1fa77c4306a052cd0a9b9273322df93262
RasitAkay11 <50198490+RasitAkay11@users.noreply.github.com>
2021-06-25 12:35:01 +0200

    Update client6.c

--- client6.c
+++ client6.c
@@ -95,6 +95,12 @@ int main(int argc, char * argv[]){
             zmq_send(publisher, "guessit>join!>Player6 is back", 29, 0);
             zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, "guessit>join?>", 14);
             printf("The game has ended.. The service is starting a new game.\n\n");
+            zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "guessit>tip?>", 13);
+            memset(buffer,0,256);
+            zmq_recv(subscriber, buffer, 256,0);
+            ParsedString = parse(3, buffer);
+            printf("%s\n\n", ParsedString);
+            zmq_setsockopt(subscriber, ZMQ_UNSUBSCRIBE, "guessit>tip?>", 13);
             zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "guessit>gok6?>", 14);
             round = 0;
         }
